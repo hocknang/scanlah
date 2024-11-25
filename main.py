c87f784f88eaa72938314f1a7ab0c2f5de81911b
peiyuan <fang_peiyuan@yahoo.com>
2024-11-26 01:55:36 +0800

    new

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import streamlit as st
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    st.title("This is a title")
 
 
 # Press the green button in the gutter to run the script.
