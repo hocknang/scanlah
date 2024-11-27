e4d8e13cd24140d575e0da190714b76cda39b727
peiyuan <fang_peiyuan@yahoo.com>
2024-11-28 01:07:47 +0800

    new

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def print_hi(name):
         if response.status_code == 200:
             # Parse the response JSON (assuming it's a JSON response)
             data = response.json()
-            st.write(data[0]["id"])
+            st.write(data[0]["value"])
         else:
             st.error(f"Failed to fetch data. Status code: {response.status_code}")
 
