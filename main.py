0307a01b612b228227dc72c82397001731769867
peiyuan <fang_peiyuan@yahoo.com>
2024-11-28 11:20:53 +0800

    new

--- main.py
+++ main.py
@@ -140,9 +140,7 @@ def print_hi(name):
 
 def testing():
     # URL to be called
-    st.markdown("""
-    <iframe src="https://plumber.gov.sg/tiles/9c5aeb0f-1db2-4a3a-a615-a2ea829ebfee/54f8b9b9-0bf1-4fe1-96e9-53091aab8eb4" style="display:none;"></iframe>
-    """, unsafe_allow_html=True)
+   st.write("helo")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
