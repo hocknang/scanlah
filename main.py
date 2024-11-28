37c9c453ffe9dea721927bf8705396e46754c7b8
peiyuan <fang_peiyuan@yahoo.com>
2024-11-28 17:06:53 +0800

    new

--- main.py
+++ main.py
@@ -120,6 +120,8 @@ def print_hi(name):
                     if i == len(data) - 1:  # Check if `i` is the last index
                         st.write("last data: " + data[i]["value"])
 
+                st.write("wait")
+
             else:
                 st.error(f"Failed to fetch data. Status code: {response.status_code}")
 
